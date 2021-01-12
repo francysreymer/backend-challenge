e8c783da1ff2e5153819e23272e46337a4e8d8af
Francys Reymer <francys.reymer@gmail.com>
2021-01-12 19:38:43 -0400

    migration complete

--- database/migrations/2021_01_12_022147_create_users_table.php
+++ database/migrations/2021_01_12_022147_create_users_table.php
@@ -15,7 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('access_level');
+            $table->string('phone');
+            $table->string('password');
+            $table->string('mlearn_id')->nullable();
+            $table->boolean('active')->default(1);
             $table->timestamps();
+            $table->engine = 'InnoDB';	
         });
     }
 
